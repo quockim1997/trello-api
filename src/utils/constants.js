1551bc1a0bcbc47beb0dd48661880510929d3fa1
Nguyen Quoc Kim <quockim1997@gmail.com>
2025-09-11 19:23:01 +0700

    config_cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,9 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+// Danh sách các Domanin được truy cập vào tài nguyên của server
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173'
+  // Ví dụ sau này sẽ deploy lên domain chính thức ...vv
+]
